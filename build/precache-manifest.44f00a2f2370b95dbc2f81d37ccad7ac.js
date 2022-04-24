fb97d23744e9bffc90ba4799535f0ecc57dd82d5
Wenhao Lu s3810097 <s3810097@student.rmit.edu.au>
2022-04-24 22:55:28 +0800

    set up auth

---
+++ build/precache-manifest.44f00a2f2370b95dbc2f81d37ccad7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b378edb7056f5c1aab2",
+    "url": "/static/css/main.829316ff.chunk.css"
+  },
+  {
+    "revision": "8b378edb7056f5c1aab2",
+    "url": "/static/js/main.8b378edb.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b230b3726b62b218329b",
+    "url": "/static/css/2.3f9676d3.chunk.css"
+  },
+  {
+    "revision": "b230b3726b62b218329b",
+    "url": "/static/js/2.b230b372.chunk.js"
+  },
+  {
+    "revision": "1af8ea36c28c08d780ad3b74e426ccc4",
+    "url": "/index.html"
+  }
+];
