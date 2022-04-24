bb4eeb5a8590f9aaf7fc0a4caa86334991f7cd6c
Wenhao Lu s3810097 <s3810097@student.rmit.edu.au>
2022-04-24 18:31:46 +0800

    update aws cognito config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.min.css';
 import './index.css';
 import App from './App';
+import Amplify from "aws-amplify";
+import config from "./config";
 import * as serviceWorker from './serviceWorker';
+
+Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.region,
+      userPoolId: config.cognito.userPoolId,
+      userPoolWebClientId: config.cognito.userPoolWebClientId
+    }
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
